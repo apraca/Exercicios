a1a5f60093305228eb815d8b498b3ce2eee786b9
apraca <0906Rej@ne87>
2021-12-22 18:13:59 -0300

    Atualização do RadixSort

--- src/br/com/bootcamp/aula1/RadixSort.java
+++ src/br/com/bootcamp/aula1/RadixSort.java
@@ -52,6 +52,8 @@ public class RadixSort {
 
         System.out.println(compZero);
 
+
+
         List<String> L0 = new ArrayList<>();
         List<String> L1 = new ArrayList<>();
         List<String> L2 = new ArrayList<>();
@@ -63,6 +65,106 @@ public class RadixSort {
         List<String> L8 = new ArrayList<>();
         List<String> L9 = new ArrayList<>();
 
+        for (Integer i=0; i <= compZero.size()-1; i++) {
+            String str = compZero.get(i).substring(compZero.get(i).length() - 1);
+            switch (str){
+                case "0": L0.add(compZero.get(i));
+                            break;
+                case "1": L1.add(compZero.get(i));
+                    break;
+                case "2": L2.add(compZero.get(i));
+                    break;
+                case "3": L3.add(compZero.get(i));
+                    break;
+                case "4": L4.add(compZero.get(i));
+                    break;
+                case "5": L5.add(compZero.get(i));
+                    break;
+                case "6": L6.add(compZero.get(i));
+                    break;
+                case "7": L7.add(compZero.get(i));
+                    break;
+                case "8": L8.add(compZero.get(i));
+                    break;
+                case "9": L9.add(compZero.get(i));
+                    break;
+
+            }
+
+
+
+        }
+
+        strArr.clear();
+        strArr.addAll(L0);
+        strArr.addAll(L1);
+        strArr.addAll(L2);
+        strArr.addAll(L3);
+        strArr.addAll(L4);
+        strArr.addAll(L5);
+        strArr.addAll(L6);
+        strArr.addAll(L7);
+        strArr.addAll(L8);
+        strArr.addAll(L9);
+
+        System.out.println(strArr);
+
+        L0.clear();
+        L1.clear();
+        L2.clear();
+        L3.clear();
+        L4.clear();
+        L5.clear();
+        L6.clear();
+        L7.clear();
+        L8.clear();
+        L9.clear();
+
+        for (Integer i=0; i <= compZero.size()-1; i++) {
+          char[] penultdig = compZero.get(i).toCharArray();
+          String str;
+          str = "" + penultdig[countCarac - 2];
+            switch (str){
+                case "0": L0.add(compZero.get(i));
+                    break;
+                case "1": L1.add(compZero.get(i));
+                    break;
+                case "2": L2.add(compZero.get(i));
+                    break;
+                case "3": L3.add(compZero.get(i));
+                    break;
+                case "4": L4.add(compZero.get(i));
+                    break;
+                case "5": L5.add(compZero.get(i));
+                    break;
+                case "6": L6.add(compZero.get(i));
+                    break;
+                case "7": L7.add(compZero.get(i));
+                    break;
+                case "8": L8.add(compZero.get(i));
+                    break;
+                case "9": L9.add(compZero.get(i));
+                    break;
+
+            }
+
+
+
+        }
+
+        strArr.clear();
+        strArr.addAll(L0);
+        strArr.addAll(L1);
+        strArr.addAll(L2);
+        strArr.addAll(L3);
+        strArr.addAll(L4);
+        strArr.addAll(L5);
+        strArr.addAll(L6);
+        strArr.addAll(L7);
+        strArr.addAll(L8);
+        strArr.addAll(L9);
+
+        System.out.println(strArr);
 
 
 
